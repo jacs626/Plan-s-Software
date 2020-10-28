bb5b6b1f70584903494bb4072d550c2c86dfb976
jacs626 <51758684+jacs626@users.noreply.github.com>
2020-10-27 21:10:44 -0300

    le meti un slider carrusel

--- resources/views/store/partials/slider.blade.php
+++ resources/views/store/partials/slider.blade.php
@@ -4,23 +4,27 @@
       <li data-target="#slider" data-slide-to="1"></li>
       <li data-target="#slider" data-slide-to="2"></li>
     </ol>
+
+
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="https://universitienda.uaa.mx/img/slider5.jpg " class="d-block w-100" alt="slide1">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="https://universitienda.uaa.mx/img/slider2.jpg" class="d-block w-100" alt="slide2">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="https://universitienda.uaa.mx/img/slider4.jpg" class="d-block w-100" alt="slide3">
       </div>
     </div>
+
+
     <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="slider" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
+  </div><hr>
